962ecbf6f8f4311ba8c96229d93c1b7a06e759df
Conner Bondurant <conner.bondurant@triptych.me>
2019-11-11 18:43:58 -0500

    Fix L block not kicking off left wall

--- src/main/java/com/github/cbondurant/Tetromino.java
+++ src/main/java/com/github/cbondurant/Tetromino.java
@@ -9,8 +9,8 @@ class TetrominoBag{
         new Tetromino( // L
             new Point [] { new Point(-1, 0), new Point(0, 0), new Point(1, 0), new Point(1, -1)},
             new Point [][] {
-                { new Point( 0, 0), new Point( 1, 0), new Point( 1, 1), new Point( 0, -2), new Point( 1, -2)},
                 { new Point( 0, 0), new Point( 0, 0), new Point( 0, 0), new Point( 0, 0), new Point( 0, 0)},
+                { new Point( 0, 0), new Point( 1, 0), new Point( 1, 1), new Point( 0, -2), new Point( 1, -2)},
                 { new Point( 0, 0), new Point( 0, 0), new Point( 0, 0), new Point( 0, 0), new Point( 0, 0)},
                 { new Point( 0, 0), new Point(-1, 0), new Point(-1, 1), new Point( 0, -2), new Point(-1, -2)}
             }, Color.ORANGE),
